79e28cec456f5ae40aad05db1554834e43180077
Teun Ouwehand <teun@nextpertise.nl>
2019-10-03 10:43:13 +0200

    Ignore system (rancher/k7s) namespaces

--- do.sh
+++ do.sh
@@ -9,6 +9,19 @@ secret=$(echo -n '{"auths":{"_ACCOUNTID_.dkr.ecr._REGION_.amazonaws.com":{"passw
 
 namespaces=$(kubectl get namespaces -o json | jq -r .items[].metadata.name)
 for namespace in ${namespaces}; do
+  echo $namespace
+  if [[ "${namespace:0:2}" == "p-" ]]; then
+    echo "skipping project namespace: $namespace"
+    continue
+  fi
+  if [[ "${namespace:0:2}" == "u-" ]]; then
+    echo "skipping user namespace: $namespace"
+    continue
+  fi
+  if [[ "${namespace:0:5}" == "user-" ]]; then
+    echo "skipping user namespace: $namespace"
+    continue
+  fi
   cat /root/registry.yaml | sed "s/_NAME_/ecr-${region}/g" | sed "s/_SECRET_/${secret}/g" | sed "s/_NAMESPACE_/${namespace}/g" > /root/generated.yaml
   kubectl apply -f /root/generated.yaml
 done
